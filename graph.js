864baeff925cdb1fa989c6e3116465d65deb43ff
skyhills13 <skyhills13@gmail.com>
2014-08-24 14:56:58 +0900

    #5 change graph upside down and customize interval

--- graph.js
+++ graph.js
@@ -1,36 +1,43 @@
 //#TODO# 
 //1. canvas 크기를 축 이름을 넣을 수 있도록 변경하고, 변수 수정
 //2. 그래프 라인 그리기
-//
+//3. 그래프 뒤집기 
+//. canvas 배경에 더 큰 canvas나 div를 둬서 모양 예쁘게 
 
 
 //그래프의 원래 크기는 800*400
-//하지만 축을 표시해야하므로, 830*430으로 캔버스의 사이즈 지정 ( html 상에서 )
-var tempJson = {"0" : "100", "1" : "300","2":"200","3" : "360", "4":"300", 5: "400"};
+//하지만 축을 표시해야하므로, 860*430으로 캔버스의 사이즈 지정 ( html 상에서 )
+var tempJson = {0 : "100", 1 : "300", 2:"200", 3 : "360", 4:"300", 5: "400"};
 
 var canvasEle = document.getElementById("polygonalGraph");
 var canvasCtx = canvasEle.getContext("2d");
-var graphWidth = canvasEle.getAttribute("width");
-var graphHeight = canvasEle.getAttribute("height");
-var bumperSize = 30;
+var canvasWidth = canvasEle.getAttribute("width");
+var canvasHeight = canvasEle.getAttribute("height");
+var xBumperSize = 60;
+var yBumperSize = 30;
+var graphWidth = canvasWidth - xBumperSize;
+var graphHeight = canvasHeight - yBumperSize;
+
+var dataLength = Object.keys(tempJson).length;
 
 var drawPoly = function( context ,json, xMagnification ) {
-
+	context.font= "16px Helvetica";
+	context.textAlign="center";
  	//graph그리기
-  	for( var i = 0 ; i < Object.keys(json).length ; ++i) {
+  	for( var i = 0 ; i < dataLength ; ++i) {
   		context.beginPath();
 		canvasCtx.lineWidth = 2;
 		canvasCtx.lineCap = "round";
     	context.strokeStyle = "#16DFF5";
-    	context.moveTo(i*xMagnification+bumperSize, parseInt(json[i]));
-    	context.lineTo((i+1)*xMagnification+bumperSize, parseInt(json[i+1]));
+    	context.moveTo(i*xMagnification+xBumperSize, graphHeight-parseInt(json[i]));
+    	context.lineTo((i+1)*xMagnification+xBumperSize, graphHeight-parseInt(json[i+1]));
   		context.stroke();
   	}
 
 	//점 표시하기
   	for ( index in json ) {
   		context.beginPath();
-    	context.arc(index*xMagnification+bumperSize,parseInt(json[index]),3,0*Math.PI,2*Math.PI);
+    	context.arc(index*xMagnification+xBumperSize,graphHeight-parseInt(json[index]),3,0*Math.PI,2*Math.PI);
   		context.fillStyle="#FFFFFF";
   		context.fill();
 		canvasCtx.lineWidth = 2;
@@ -38,16 +45,41 @@ var drawPoly = function( context ,json, xMagnification ) {
     	context.strokeStyle = "#16DFF5";
   		context.stroke();
   	}
-
+  	//축 변수표시하기
+  	//x축 
   	for ( index in json ) {
-  		context.font= "20px Helvetica";
-  		context.textAlign="center";
   		context.fillStyle = "#3B373B"
-  		context.fillText(index.toString(), index*xMagnification+bumperSize, graphHeight);
+  		context.fillText(index.toString(), index*xMagnification+xBumperSize, canvasHeight);
   	}
+  	//y축
+  	for( var i = 0 ; i < 8 ; ++i) {
+  		context.textBaseline = "middle";
+  		context.fillStyle = "#3B373B"
+  		//TODO 100이랑 30도 비율로 계산해서 변수로 뺄 것 
+  		context.fillText(i*100, 0+xBumperSize-30, graphHeight-i*100);
+  	}
+  
+  	//모눈종이 그리기
+  	//수평방향 
+  	context.lineWidth = 1;
+  	context.strokeStyle = "#D7E4FA";
+  	context.beginPath();
+    for (var i = 0; i <= graphHeight ; i += 100 ) {
+    	context.moveTo(0+xBumperSize, i);
+    	context.lineTo(graphWidth+xBumperSize, i);
+    }
+    context.stroke();
+
+	//수직방향 
+    context.beginPath();
+    for (var i = 0 ; i < graphWidth ; i += graphWidth/dataLength ) {
+    	context.moveTo(i+xBumperSize, 0);
+    	context.lineTo(i+xBumperSize, graphHeight);
+    }
+    context.stroke();
 }
 
-drawPoly(canvasCtx, tempJson, 100);
+drawPoly(canvasCtx, tempJson, graphWidth/dataLength);
 
 
 
